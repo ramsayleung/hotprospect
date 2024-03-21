8ed4ca08c4564d8c4ae47dc42034d314bec5193a
Ramsay Leung <ramsayleung@gmail.com>
2024-03-20 19:53:50 -0700

    Initial Commit

---
+++ HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by ramsayleung on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
