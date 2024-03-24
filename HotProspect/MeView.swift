74e076f22d48ccb5c8f1d06f0fd3b72b206df3b7
Ramsay Leung <ramsayleung@gmail.com>
2024-03-24 10:46:25 -0700

    Update MeView to generate QRCode based on user information.

--- HotProspect/MeView.swift
+++ HotProspect/MeView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    
+    let ciContext = CIContext()
+    let ciFilter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("EmailAddress", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        ciFilter.message = Data(string.utf8)
+        
+        if let outputImage = ciFilter.outputImage {
+            if let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent){
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
